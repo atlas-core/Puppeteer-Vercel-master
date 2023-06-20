913b5f14cbf44e0b5a47c73dcdc80bbc7d0e8707
atlas-core <elyakoubiayman1@gmail.com>
2023-06-20 11:29:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const app = require("express")();
 const puppeteer = require("puppeteer-extra");
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
-puppeteer.use(StealthPlugin());
+
+
+
+
+
 
 const cookies = [
   {
@@ -341,6 +344,9 @@ const cookies = [
 
 
 
+
+
+
 app.get("/", async (req, res) => {
   try {
     let options = {};
@@ -363,7 +369,8 @@ app.get("/", async (req, res) => {
       await page.setCookie(cookie);
     }
  
-    await page.goto("https://www.gmail.com" ,{ timeout: 60000 });
+    await page.goto("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox");
+   
     //await page.type("#identifierId", "aymandemaroc@gmail.com", { delay: 150 });
     //await page.click("#identifierNext");
    // await page.waitForTimeout(2000);
@@ -383,6 +390,8 @@ app.get("/", async (req, res) => {
    res.send(screenshot);
 
     await browser.close();
+
+
   } catch (err) {
     console.error(err);
     res.status(500).send(err);
