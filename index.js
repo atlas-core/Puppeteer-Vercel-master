277e0addd3ca29bfa907661e242317b741b9d560
atlas-core <elyakoubiayman1@gmail.com>
2023-06-20 13:26:00 +0100

    LOL9090

--- index.js
+++ index.js
@@ -9,341 +9,54 @@ const puppeteer = require("puppeteer");
 app.get("/", async (req, res) => {
 
   
-/* const cookies = [
-  {
-    "name": "__Secure-3PSIDCC",
-    "value": "AP8dLty-csTSdBEphMf0tByPOmaD0lgQxBDtE2GDOAhw9Calq65PQH4r-LkpDaN8rgfD7CxJNQ",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1718794134.908307,
-    "size": 90,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameSite": "None",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Secure-1PSIDTS",
-    "value": "sidts-CjEBLFra0h1OW8JvtN6KbNeZ90fgELND70VQ81IGlNq0l0xDgybqtKUeTUJJwElHD0r8EAA",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1718794131.72947,
-    "size": 93,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameParty": true,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "NID",
-    "value": "511=qspo_XSZ83JwuZNBUHR8BVXxVEmCT92k5t3OTokkSh8pb1CEcACJclg0_geFT1Yo8jsSiGv9VV7VjKJilrt0dhUpMR9KXlSlQlrSWj61zIF5GLXIlhf_OJwkgym7Tji0zaCYVOOgYHoExLYp4_1-sUs8gsYo-xWerDHuLrxIkGFPOizj390wCV9TV6voiIT6ev03mOvGUW71v2IZKX9E44pyymWR428B_qUfl-NxG4jpr85qlGmcg-tHI_iQC1CWSK32F5At",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1703069329.515658,
-    "size": 271,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameSite": "None",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Host-GMAIL_SCH_GMS",
-    "value": "1",
-    "domain": "mail.google.com",
-    "path": "/",
-    "expires": 1689850127.423117,
-    "size": 21,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameSite": "Strict",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Host-GMAIL_SCH_GMN",
-    "value": "1",
-    "domain": "mail.google.com",
-    "path": "/",
-    "expires": 1689850127.423026,
-    "size": 21,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Secure-3PSIDTS",
-    "value": "sidts-CjEBLFra0h1OW8JvtN6KbNeZ90fgELND70VQ81IGlNq0l0xDgybqtKUeTUJJwElHD0r8EAA",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1718794131.729536,
-    "size": 93,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameSite": "None",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "OSID",
-    "value": "XwhDtpgRwc3WW0aIYkRtE63yS0ymk4xGQv_LxQKQAZh8vN0COWLypHwQuWTnr0uQvOEaHA.",
-    "domain": "mail.google.com",
-    "path": "/",
-    "expires": 1750330126.083463,
-    "size": 75,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Secure-1PSIDCC",
-    "value": "AP8dLtzB3VIx4MylpyWiHDJuy82SwyIy87JP7j9gYmUy7H12rCzO1HROrPxQE0F-rFR3L5O0uQ",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1718794134.908205,
-    "size": 90,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "SIDCC",
-    "value": "AP8dLtzNeSteq_pQJundTiZNACegDA5MRYusKISOz_acTyv_3urU_42LFxZ_OiTPPJrrDKlD6Q",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1718794134.908002,
-    "size": 79,
-    "httpOnly": false,
-    "secure": false,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "SEARCH_SAMESITE",
-    "value": "CgQIyJgB",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1702810129.51559,
-    "size": 23,
-    "httpOnly": false,
-    "secure": false,
-    "session": false,
-    "sameSite": "Strict",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Secure-3PAPISID",
-    "value": "3EawhNGDSoKS2T7l/Auqw2cYuppAsN16sU",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.861465,
-    "size": 51,
-    "httpOnly": false,
-    "secure": true,
-    "session": false,
-    "sameSite": "None",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "SAPISID",
-    "value": "3EawhNGDSoKS2T7l/Auqw2cYuppAsN16sU",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.861367,
-    "size": 41,
-    "httpOnly": false,
-    "secure": true,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "APISID",
-    "value": "2Y3GgJn7hUHOKF6N/AicI4sR3j4JoSUaKW",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.861324,
-    "size": 40,
-    "httpOnly": false,
-    "secure": false,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "SSID",
-    "value": "AUJWG3KqepAEmFLvJ",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.861281,
-    "size": 21,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Secure-1PAPISID",
-    "value": "3EawhNGDSoKS2T7l/Auqw2cYuppAsN16sU",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.861418,
-    "size": 51,
-    "httpOnly": false,
-    "secure": true,
-    "session": false,
-    "sameParty": true,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "HSID",
-    "value": "A8IwSasLmXuPoUWgx",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.861235,
-    "size": 21,
-    "httpOnly": true,
-    "secure": false,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Host-GMAIL_SCH_GML",
-    "value": "1",
-    "domain": "mail.google.com",
-    "path": "/",
-    "expires": 1689850127.423189,
-    "size": 21,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameSite": "Lax",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Secure-1PSID",
-    "value": "XwhDtobqC9fAxf08l6VtIFoSdL11jiwEY1iD8Obgz9vQTzHl_HbCvql9mx4c5guGxnyq9Q.",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.860976,
-    "size": 85,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameParty": true,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "SID",
-    "value": "XwhDtobqC9fAxf08l6VtIFoSdL11jiwEY1iD8Obgz9vQTzHl98542sgN4Ys3bMUqZqMywQ.",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.860925,
-    "size": 74,
-    "httpOnly": false,
-    "secure": false,
-    "session": false,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "COMPASS",
-    "value": "gmail=CqMBAAlriVf14ubeRhvJkSlJwC4Q78QJWyBFHWoYYZwRsA2TlsyD22Ce1eYUWTAU_3YbG2fLkf0hn3udcZJVJ1KysDSFMc2yYwD28e7GHV5uIxJdZoDd_Ia83bSD1QZpYv_H0yKdZYaRvckpNj-NGg2uKXHxNR5WRTvxhcyGSlYs9UQaI8QjtzqmDgiQEyltw-bU0DgNoWnXC9lRvBdLZlYqevrFeBD9o8akBhrKAQAJa4lXKc2LimMD8UynYP-7c4o2GFWtpdXH8cCym_-n4t5BC389-4Xdjci0IF5H4bY0zcT00uDsSlWIeAlVBWHwsMq1aoY3sP5jEjcAT4jcVwq99eGdyHk8SwK1zaLz7oKVu8CQ2Pzr-Qmxh7nLpraLxiExH8b1RL0XjPtskSiAy_hDnsE66xau3u98T42DB-S1si_pJYZzsFypxdyZ0J1z14EqkqnIWzMaPy2L85HL_Y2SpXZr1oqiMLAj4xz0HM1QdcnUMTmUDN8wAQ:gmail_ps=CpABAAlriVd7UDpeS1eQ7WW2qPpexAAmqzNMaQ3EBKQY17VrBHN1xKRHiMP8AdUlqq427xm7UPwc7sY2IZSqpvTxv_OJVomN0uRknhRXiNGbVzlLc8CJIVTpclUT8KNyANgNjkJKHCZJm3BBCPaaVs3uPTBo7FQZOfZJN9ZpZyDJJf45CiLu_FDFu3F6mrYGUcFwENSkxqQGGsYBAAlriVd1RSP-Lqfb4Us7Xr-PQIOOAvQaNx8HHgbFG0Eq2lUEK6uFhfwB-WI60tBkJt6sLCthNzdVhxxef5QM3pdSyWMCTJYSc6V7K1JAMT-fiqMp8mG3-UQkl_7SUyUVQ88GjhPWEM50bxyPUQ4BCNg4oRCtN8DFaqCH0ZTNzfZcWJ3HONC--ZSAoDVxoSjpAkYhyeAAED_GW2zlOHgr6-1gs9ToYekksIbdAobRW4f2OLLexiNOn4YvhjsfIheC9KNomvvGMAE",
-    "domain": "mail.google.com",
-    "path": "/mail",
-    "expires": 1688122126.947481,
-    "size": 1004,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameSite": "None",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Secure-OSID",
-    "value": "XwhDtpgRwc3WW0aIYkRtE63yS0ymk4xGQv_LxQKQAZh8vN0CMTRd4WA0IEsNVJhio47pIA.",
-    "domain": "mail.google.com",
-    "path": "/",
-    "expires": 1750330126.08355,
-    "size": 84,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameSite": "None",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "__Secure-3PSID",
-    "value": "XwhDtobqC9fAxf08l6VtIFoSdL11jiwEY1iD8Obgz9vQTzHl4AwttK-0Alj6HWoSimz2SQ.",
-    "domain": ".google.com",
-    "path": "/",
-    "expires": 1750330125.861055,
-    "size": 85,
-    "httpOnly": true,
-    "secure": true,
-    "session": false,
-    "sameSite": "None",
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  },
-  {
-    "name": "GMAIL_AT",
-    "value": "AF6bupO_Z9ySg1rIkdH_5flJTNsdPPMmIg",
-    "domain": "mail.google.com",
-    "path": "/mail/u/0",
-    "expires": -1,
-    "size": 42,
-    "httpOnly": false,
-    "secure": true,
-    "session": true,
-    "sameParty": false,
-    "sourceScheme": "Secure",
-    "sourcePort": 443
-  }
-]
- */
+  const cookies = [
+    {
+      "name": "OSID",
+      "value": "XwhDtgUsSAt7Kp5wQIbeQosse1p9mgncdXgnveAJq06TC3oPtQflbDaxc-0mxIHrdnntnA.",
+      "domain": "mail.google.com",
+      "path": "/",
+      "expires": 1750259568.516582,
+      "size": 75,
+      "httpOnly": true,
+      "secure": true,
+      "session": false,
+      "sameParty": false,
+      "sourceScheme": "Secure",
+      "sourcePort": 443
+    },
+    {
+      "name": "__Secure-1PSID",
+      "value": "XwhDtn-ISXKJ7C4mU7qOQ79KDIKGHyKaRtu6VulZIzPQPHHxX9klXQxgHFigBmPA1j-2WA.",
+      "domain": ".google.com",
+      "path": "/",
+      "expires": 1750259568.241814,
+      "size": 85,
+      "httpOnly": true,
+      "secure": true,
+      "session": false,
+      "sameParty": true,
+      "sourceScheme": "Secure",
+      "sourcePort": 443
+    },
+    {
+      "name": "SID",
+      "value": "XwhDtn-ISXKJ7C4mU7qOQ79KDIKGHyKaRtu6VulZIzPQPHHxP7GzNAuizrkUlhK0g8E6JQ.",
+      "domain": ".google.com",
+      "path": "/",
+      "expires": 1750259568.241746,
+      "size": 74,
+      "httpOnly": false,
+      "secure": false,
+      "session": false,
+      "sameParty": false,
+      "sourceScheme": "Secure",
+      "sourcePort": 443
+    }
+   
+  ]
+  
+  
+ 
   
 
   try {
@@ -362,10 +75,10 @@ app.get("/", async (req, res) => {
 
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
-   /*  for (const cookie of cookies) {
+     for (const cookie of cookies) {
       await page.setCookie(cookie);
     }    
- */
+ 
   
  
     await page.goto("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox");
